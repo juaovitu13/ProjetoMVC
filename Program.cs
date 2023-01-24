b1bc4ef3afc50c85c5966a77c69f1e9fa89f2177
juaovitu13 <joao.victorsoares@outlook.com>
2023-01-24 15:45:05 -0300

    Migrations criada

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoMVC.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AgendaContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -8,9 +14,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+   app.UseExceptionHandler("/Home/Error");
+   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+   app.UseHsts();
 }
 
 app.UseHttpsRedirection();
